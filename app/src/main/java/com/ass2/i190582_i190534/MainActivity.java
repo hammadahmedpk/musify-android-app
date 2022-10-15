d85eec2f17c4eb98fd73911efb033d8628f0ac88
Hammad Ahmed <hammadahmedpk1@gmail.com>
2022-10-15 15:00:28 +0500

    Add: Splash Screen Animation

--- app/src/main/java/com/ass2/i190582_i190534/MainActivity.java
+++ app/src/main/java/com/ass2/i190582_i190534/MainActivity.java
@@ -1,6 +1,12 @@
 package com.ass2.i190582_i190534;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For slider animation
+        ImageView image = findViewById(R.id.splashLogo);
+        TextView title = findViewById(R.id.title);
+        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_screen);
+        image.startAnimation(slideAnimation);
+        title.startAnimation(slideAnimation);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MusicLibrary.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000L);
+
     }
 }
+
