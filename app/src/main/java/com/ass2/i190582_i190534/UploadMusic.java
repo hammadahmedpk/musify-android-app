814ca60bb9bdad44540520f257e9a516817d8ce1
UswaNasir1 <i190534@nu.edu.pk>
2022-10-18 10:29:42 +0500

    Add: Seekbar functionality to play songs

--- app/src/main/java/com/ass2/i190582_i190534/UploadMusic.java
+++ app/src/main/java/com/ass2/i190582_i190534/UploadMusic.java
@@ -1,10 +1,14 @@
 package com.ass2.i190582_i190534;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +29,14 @@ import com.google.firebase.storage.UploadTask;
 public class UploadMusic extends AppCompatActivity {
     ImageView backButton, uploadMusic;
     Button recordBtn, uploadBtn;
+    EditText title, genre, description;
     private final int PICK_AUDIO = 1;
     Uri AudioUri;
     FirebaseDatabase db;
     FirebaseAuth mAuth;
     FirebaseStorage storage;
     StorageReference storageRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,10 @@ public class UploadMusic extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         uploadMusic = findViewById(R.id.upload_music);
         backButton = findViewById(R.id.backarrow);
+        title = findViewById(R.id.title);
+        genre = findViewById(R.id.genre);
+        description = findViewById(R.id.description);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,17 +80,28 @@ public class UploadMusic extends AppCompatActivity {
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              // Toast.makeText(UploadMusic.this, "Failed to upload image URL to database!", Toast.LENGTH_SHORT).show();
-                storageRef.child("audios").child(mAuth.getUid()).child("uploaded_songs").child("song01").putFile(AudioUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                Song song = new Song(title.getText().toString(), genre.getText().toString(), description.getText().toString());
+                Toast.makeText(UploadMusic.this, "Uploading Song!", Toast.LENGTH_SHORT).show();
+                storageRef.child("audios").child(mAuth.getUid()).child("uploaded_songs").child(song.title).putFile(AudioUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                         if (task.isSuccessful()) {
+                            task.getResult().getStorage().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    db.getReference().child("Songs").child(song.title).child("Song").setValue(uri.toString());
+                                }
+                            });
                             Toast.makeText(UploadMusic.this, "Music has been uploaded!", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(UploadMusic.this, "Failed to upload image URL to database!", Toast.LENGTH_SHORT).show();
                        }
                     }
                 });
+
+                db.getReference().child("Songs").child(song.title).child("Description").setValue(song.description);
+                db.getReference().child("Songs").child(song.title).child("Genre").setValue(song.genre);
+                db.getReference().child("Songs").child(song.title).child("Title").setValue(song.title);
             }
         });
     }
@@ -90,12 +111,13 @@ public class UploadMusic extends AppCompatActivity {
 
         if (requestCode == PICK_AUDIO && resultCode == RESULT_OK) {
             AudioUri = data.getData();
-            Toast.makeText(UploadMusic.this, "Audio has been added!", Toast.LENGTH_SHORT).show();
-
+            //String name = getNameFromURI(AudioUri);
+            Toast.makeText(UploadMusic.this, "Added Song!", Toast.LENGTH_SHORT).show();
         }
     }
     private void startRecordMusic() {
         Intent switchActivityIntent = new Intent(this, com.ass2.i190582_i190534.RecordMusic.class);
         startActivity(switchActivityIntent);
     }
+
 }
